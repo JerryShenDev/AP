aa5a780237d16db40d8837bd7dc1e1cb9ee9f651
jza229 <jza229@sfu.ca>
2024-04-21 20:02:26 -0700

    Topic up to 5.6

--- src/Unit8_2D_Array/Array2DTraversal.java
+++ src/Unit8_2D_Array/Array2DTraversal.java
@@ -29,21 +29,54 @@ public class Array2DTraversal {
     }
 
     private static void printColumnOrder(int[][] matrix) {
-        for (int j = 0; j < matrix[0].length; j++) {
+        // find the largest row size
+        int maxSize = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            maxSize = Math.max(matrix[i].length, maxSize);
+        }
+
+        for (int j = 0; j < maxSize; j++) {
             for (int i = 0; i < matrix.length; i++) {
-                System.out.print(matrix[i][j] + " ");
+                // check whether j exceeds the row size
+                if (j < matrix[i].length) {
+                    System.out.print(matrix[i][j] + " ");
+                }
             }
         }
         System.out.println();
     }
 
     private static void printColumnReverseOrder(int[][] matrix) {
-        for (int j = matrix[0].length - 1; j >= 0; j--) {
+        // find the largest row size && which row it is
+        int maxSize = 0;
+        int maxSizeRow = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i].length >= maxSize) {
+                maxSize = matrix[i].length;
+                maxSizeRow = i;
+            }
+        }
+
+        for (int j = maxSize - 1; j >= 0; j--) {
             for (int i = matrix.length - 1; i >= 0; i--) {
-                System.out.print(matrix[i][j] + " ");
+                if (j <= matrix[i].length - 1) {
+                    System.out.print(matrix[i][j] + " ");
+                }
             }
         }
         System.out.println();
     }
-}
+
+    public static void main(String[] args) {
+        int[][] intArray = {
+                {1, 2},
+                {3, 4},
+                {5, 6, 7},
+                {8},
+                {9, 10}
+        };
+//        printColumnOrder(intArray);
+
+        printColumnReverseOrder(intArray);
+    }
 }
