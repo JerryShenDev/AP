a67d46ea6dc068c31e2e5addfbd98ab46c5f7276
jerryshen <2530356192@qq.com>
2024-04-21 21:09:57 -0700

    al

---
+++ src/Practice2017/DigitsMain.java
@@ -0,0 +1,9 @@
+package Practice2017;
+
+public class DigitsMain {
+    public static void main(String[] args) {
+        Digits digits1 = new Digits();
+        System.out.println(digits1.getDigitList());
+        System.out.println(digits1.isStrictlyIncreasing());
+    }
+}
